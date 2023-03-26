6424719941f5013225b19fd205d27161dc011583
TIME <opout777@yandex.ru>
2023-03-26 19:01:50 +0300

    First commit

---
+++ src/main/java/pro/sry/homework/calculator/calculatorForTests/CalculatorForTestsApplication.java
@@ -0,0 +1,13 @@
+package pro.sry.homework.calculator.calculatorForTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorForTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorForTestsApplication.class, args);
+	}
+
+}
